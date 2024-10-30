372a48282e2f2b358d3e58e46ca3db010c78e0d3
glitch-Staticize <static.staticize@gmail.com>
2024-10-30 14:09:56 -0400

    Added some function to Asymptose finder

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 function cosineLaw(sideA, sideB, angleC) {
     return Math.sqrt(sideA * sideA + sideB * sideB - 2 * sideA * sideB * Math.cos(angleC));
 }
+document.getElementById("calculateCosine").onclick = function (event) {
+    event.preventDefault();
 
-document.getElementById("calculateCosine").onclick = function() {
-    var sideA = parseInt(document.getElementById("sideA").value);
-    var sideB = parseInt(document.getElementById("sideB").value);
-    var angleC = parseInt(document.getElementById("angleC").value);
+    var sideA = parseFloat(document.getElementById("sideA").value);
+    var sideB = parseFloat(document.getElementById("sideB").value);
+    var angleC = parseFloat(document.getElementById("angleC").value) * (Math.PI / 180);
 
     var result = cosineLaw(sideA, sideB, angleC);
-    document.getElementById("cosineResult").innerText=result.toFixed(2);
+    document.getElementById("cosineResult").innerText = result;
 }
 
-    
+function findAsymptote(nValue, mValue) {
 
+}
+document.getElementById("findAsymptote").onclick = function (event) {
+    event.preventDefault();
 
-
-function asymptoteFinder(mValue, nValue)
+    var nValue = parseFloat(document.getElementById("nValue").value);
+    var sideB = parseFloat(document.getElementById("sideB").value);
+   
+    var result = cosineLaw(sideA, sideB, angleC);
+    document.getElementById("cosineResult").innerText = result.toFixed(2);
+}
